6d42bd52cf7dccf7ff26b1e24346929f795c54fd
Daniel Cerón <62627348+DanielCeron-dc@users.noreply.github.com>
2021-03-16 19:18:58 -0500

    SelectInInterval0.1

--- lib/Games/SelectInInterval/Widgets/DropContainer.dart
+++ lib/Games/SelectInInterval/Widgets/DropContainer.dart
@@ -1,4 +1,30 @@
+
 import 'package:flutter/material.dart';
 
+class DropContainer extends StatefulWidget {
 
 
+  @override
+  _DropContainerState createState() => _DropContainerState();
+}
+
+class _DropContainerState extends State<DropContainer> {
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height *.4;
+    return Container(
+      width: double.infinity,
+      height: height,
+      
+      decoration: BoxDecoration(
+        
+        color: Colors.white,
+        // Red border with the width is equal to 5
+        border: Border.all(
+            width: 10, 
+            color: Color.fromRGBO(114,211,227,1)
+      )),
+      child: Center(child:Text("dasd")),
+    );
+  }
+}
